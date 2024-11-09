51d6490743a629775b90faab0649a2790e00e6fb
Schoeneh <116974162+Schoeneh@users.noreply.github.com>
2024-11-09 23:40:44 +0100

    :bug: fixed replacing and saving in/of tag

--- search&replace/search_replace.js
+++ search&replace/search_replace.js
@@ -1,17 +1,19 @@
 var fieldName = prompt("Which field should be searched?\n\nFor a list of all available fields see:\nhttps://api.zotero.org/itemFields", "title");
 var fieldID = Zotero.ItemFields.getID(fieldName);
-if (fieldName.includes("tag")){fieldName = "tag"}
+var Tag = false;
+if (fieldName.includes("tag")){fieldName = "tag", Tag = true}
 else if (!fieldID) {alert("Error: \""+fieldName+"\" is not a valid field."); return undefinded}
 
 var search = prompt("What characters/words should be searched for?", "Foo");
 var replace = prompt("What should it be replaced with?", "Foobar");
 
 const date = new Date(Date.now())
-const tag = "SnR_"+date.toISOString()
+const uniqueTag = "SnR_"+date.toISOString()
 // Search
 try {
     var s = new Zotero.Search();
     s.libraryID = ZoteroPane.getSelectedLibraryID();
+    
     if (fieldName.includes("date")){
         s.addCondition(fieldName, 'is', search);
     }
@@ -27,21 +29,19 @@ try {
         var fieldValue = item.getField(fieldName);
         if (fieldValue.includes(search)) {idsCorrect.push(id);}
     }
-    if (fieldName == "tag"){idsCorrect = ids}
+    if (Tag){idsCorrect = ids}
 
     // Preview of Edit
     if (!idsCorrect.length) {alert("No items found")}
     else {
         var previewItem = await Zotero.Items.getAsync(idsCorrect[0]);
-        var previewOldValue = ""
-        //return previewItem;
-        if (fieldName == "tag") {
+        if (Tag) {
             let previewTags = previewItem.getTags();
             for (let element of previewTags){
-                if (element.tag.includes(search)){previewOldValue = element.tag}
+                if (element.tag.includes(search)){var previewOldValue = element.tag}
             }
         }
-        else {previewOldValue = previewItem.getField(fieldName)}
+        else {var previewOldValue = previewItem.getField(fieldName)}
         
         let previewNewValue = previewOldValue.replace(search, replace);
         var confirmed = confirm(idsCorrect.length + " item(s) found" + "\n\n" +
@@ -50,18 +50,33 @@ try {
 
     // Replace
     if (confirmed == true) {
-        await Zotero.DB.executeTransaction(async function () {
+
             for (let id of idsCorrect) {
                 let item = await Zotero.Items.getAsync(id);
-                item.addTag(tag);
-                let mappedFieldID = Zotero.ItemFields.getFieldIDFromTypeAndBase(item.itemTypeID, fieldName);
-                let oldValue = item.getField(fieldName);
-                let newValue = oldValue.replace(search, replace);
-                item.setField(mappedFieldID ? mappedFieldID : fieldID, newValue);
-                await item.save();
+                item.addTag(uniqueTag);
+                if (Tag){
+                    let itemTags = item.getTags();
+                    for (let element of itemTags){
+                        if (element.tag.includes(search)){
+                            let oldValue = element.tag;
+                            let newValue = oldValue.replace(search, replace);
+                            item.addTag(newValue);
+                            item.removeTag(oldValue);
+                            await item.save()
+                        }
+                    }
+                }
+                else {
+                    await Zotero.DB.executeTransaction(async function () {
+                        let mappedFieldID = Zotero.ItemFields.getFieldIDFromTypeAndBase(item.itemTypeID, fieldName);
+                        let oldValue = item.getField(fieldName);
+                        let newValue = oldValue.replace(search, replace);
+                        item.setField(mappedFieldID ? mappedFieldID : fieldID, newValue);
+                        await item.save();
+                })
             }
-        });
-        alert(idsCorrect.length + " item(s) updated.\n(See tag: "+tag+")");
+        };
+        alert(idsCorrect.length + " item(s) updated.\n(See tag: "+uniqueTag+")");
     }
 }
 catch(err) {alert(err)}
