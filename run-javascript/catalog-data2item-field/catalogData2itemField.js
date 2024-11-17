9de72e11d4c2cf8754b6d4ca561bbbf91aa1b7cd
Schoeneh <116974162+Schoeneh@users.noreply.github.com>
2024-11-17 20:15:26 +0100

    finished: config as json; still working on polishing

--- run-javascript/catalog-data2item-field/catalogData2itemField.js
+++ run-javascript/catalog-data2item-field/catalogData2itemField.js
@@ -1,52 +1,61 @@
 // API = https://wiki.k10plus.de/display/K10PLUS/SRU
 // https://services.dnb.de/sru/dnb?version=1.1&operation=searchRetrieve&query=NUM%3D978-3-8353-0636-3&recordSchema=MARC21-xml
 
-const config = {"rvk_kobv":[{
-    "apiPrefix":"https://sru.kobv.de/k2?version=1.1&operation=searchRetrieve&query=dc.identifier%3D",
-    "apiSuffix":"&startRecord=1&maximumRecords=10&recordSchema=marcxml&recordPacking=xml&stylesheet=",
-    "namespace1":"http://www.loc.gov/zing/srw/",
-    "namespace2":"http://www.loc.gov/MARC21/slim",
-    "eval1":"//zs:records/zs:record/zs:recordData",
-    "eval2":"//datafield[@tag='084' and subfield[@code='2'] = 'rvk']/subfield[@code='a']"}
-]};
-
+const config = {
+    "rvk_kobv":{
+        "apiPrefix":"https://sru.kobv.de/k2?version=1.1&operation=searchRetrieve&query=dc.identifier%3D",
+        "apiSuffix":"&startRecord=1&maximumRecords=10&recordSchema=marcxml&recordPacking=xml&stylesheet=",
+        "namespace1":"http://www.loc.gov/zing/srw/",
+        "namespace2":"http://www.loc.gov/MARC21/slim",
+        "eval1":"//zs:records/zs:record/zs:recordData",
+        "eval2":"//datafield[@tag='084' and subfield[@code='2'] = 'rvk']/subfield[@code='a']"},
+};
+var selection = false;
+var search = false;
 const scope = prompt("Edit selected items oder search for items to edit? (selected/search)", "selected");
-if (scope.includes("sel")){const selection = true}
-else if (scope.includes("sea")){const search = true}
+if (scope.includes("sel")){selection = true}
+else if (scope.includes("sea")){search = true}
 else {alert("<Error>: \""+scope+"\" is not a valid input.")}
 
 var parser = new DOMParser();
 const date = new Date(Date.now());
 
-const operation = prompt(("Which operation to perform?\n" + list_operations(pathAPIs)), Object.keys(pathAPIs)[0])
+const operation = prompt(("Which operation to perform?\n" + list_operations(config)), String(Object.keys(config)[0]))
 try {
-    apiPrefix = Object.values(pathAPIs)[operation];
-    apiSuffix = Object.values(pathAPIs)[operation];
-    namespace1() = Object.values(pathAPIs)[operation];
-    namespace2 = Object.values(pathAPIs)[operation];
-    eval1 = Object.values(pathAPIs)[operation];
-    eval2 = Object.values(pathAPIs)[operation];
+    var apiPrefix = config[operation]['apiPrefix'];
+    var apiSuffix = config[operation]['apiSuffix'];
+    var eval1 = config[operation]['eval1'];
+    var eval2 = config[operation]['eval2'];
+} catch(err) {alert("<Error>: \""+operation+"\" is not a valid operation.\n\n"+err)}
 
-} catch {alert("<Error>: \""+operation+"\" is not a valid operation.")}
-
+// Getting ISBN (and dealing with null/invalid)
+var coll_err_isbn_invalid = new Zotero.Collection();
+coll_err_isbn_invalid.name = operation + "_Err:ISBNinvalid_" + date.toISOString();
+var coll_err_isbn_null = new Zotero.Collection();
+coll_err_isbn_null.name = operation + "_Err:ISBNnull_" + date.toISOString();
 try {
     if (selection){
         var items = Zotero.getActiveZoteroPane().getSelectedItems();
+        var queryItems = [];
+        var invalidQueryItems = 0;
+        var noQueryItems = 0;
+        for (let item of items) {
+            try {
+                var query = item.getField('ISBN');
+                query = query.replace(/-/g, "");
+                query = query.trim();
 
+                if (query.length <= 13) {queryItems.push(item)
+                } else {invalidQueryItems++}
+            } catch {noQueryItems++}
+            }
+            confirm("Selected "+String(queryItems.length + invalidQueryItems + noQueryItems)+" items:\n" + String(queryItems.length) + "x valid ISBN,\n" + String(invalidQueryItems) + "x invalid ISBN,\n" + String(noQueryItems) + "x no ISBN\n\nContinue?");
+        } else if (search){alert("<Error> Not yet implemented. Sorry!\nSee https://github.com/Schoeneh/zotero_scripts/issues/19")}
+    } catch(err) {alert("<Error>\n"+err+"\n\n Please open an issue at https://github.com/Schoeneh/zotero_scripts/issues!")}
 
-    }
-    else if (search){alert("<Error> Not yet implemented. Sorry!")}
-} catch(err) {alert("<Error>\n"+err+"\nscope: "+scope+"\nfieldName: "+fieldName+"\n"+"search: "+search+"\nreplace: "+replace+"\n\nFeel free to open an issue at https://github.com/Schoeneh/zotero_scripts/issues and include this error-message.")}
-
-
-var collection_error = new Zotero.Collection();
-collection_error.name = "ISBN2RVK_Error_" + date.toISOString()
-
-var collection_success = new Zotero.Collection();
-collection_success.name = "ISBN2RVK_" + date.toISOString()
 
 for (let item of items) {
-    var txt = "";
+    //var txt = "";
     var query = item.getField('ISBN');
     if (!query){
         await collection_error.saveTx();
@@ -59,13 +68,13 @@ for (let item of items) {
         
         let data = await Zotero.HTTP.request("GET", apiCall + query + suffix);
         let xmlDoc = data.responseXML;
-        let test = xmlDoc.evaluate("//zs:records/zs:record/zs:recordData",xmlDoc,namespace1,XPathResult.ANY_TYPE,null);
+        let test = xmlDoc.evaluate("//zs:records/zs:record/zs:recordData",xmlDoc,namespace1(config, operation),XPathResult.ANY_TYPE,null);
     
         let node = null;
         var RVK = [];
         while ((node = test.iterateNext())) {
             var xmlDoc2 = parser.parseFromString(node["innerHTML"],"text/html");
-            var test2 = xmlDoc2.evaluate("//datafield[@tag='084' and subfield[@code='2'] = 'rvk']/subfield[@code='a']",xmlDoc2,namespace2,XPathResult.ANY_TYPE,null);
+            var test2 = xmlDoc2.evaluate("//datafield[@tag='084' and subfield[@code='2'] = 'rvk']/subfield[@code='a']",xmlDoc2,namespace2(config, operation),XPathResult.ANY_TYPE,null);
         
             while ((node2 = test2.iterateNext())) {
                 RVK.push(node2["innerHTML"]);
@@ -80,15 +89,12 @@ for (let item of items) {
 }
 
 function list_operations(pathAPIs) {
-    for (key in Object.keys(pathAPIs)){
-        return Object.keys(pathAPIs)[key]+"\n"
-    }
-
+    for (key in Object.keys(pathAPIs)){return Object.keys(pathAPIs)[key]+"\n" }
 }
 
-function namespace1() {
-  return "http://www.loc.gov/zing/srw/";
+function namespace1(config, operation) {
+  return Object.values(config)[operation]['namespace1']
 }
-function namespace2() {
-    return "http://www.loc.gov/MARC21/slim"
+function namespace2(config, operation) {
+    return Object.values(config)[operation]['namespace2']
 }
